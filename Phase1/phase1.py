467c2ce130618a4c0543f0090865a24852b31429
kruti3 <kruti.chauhan.93@gmail.com>
2016-12-07 20:47:18 -0500

    Update phase1.py

--- Phase1/phase1.py
+++ Phase1/phase1.py
@@ -1,91 +1,100 @@
 import os
-import extract_features
-import extract_goldlabel
+import extract_featurevector
+import extract_goldlabels
 from sklearn import linear_model
 from sklearn import metrics 
 import pickle
 
-class Classifier:
 
-	def __init__(self):
+class DateExtraction:
+
+    def __init__(self):
             
-            self.DATA_PATH = '\DataSet\Timeline17\Data'
-            self.METADATA_PATH = '\DataSet\Timeline17\MetaData'
-            
-            self.topic_list = ['bpoil_bbc','bpoil_foxnews','bpoil_guardian','bpoil_reuters','bpoil_washingtonpost','EgyptianProtest_cnn','Finan_washingtonpost','H1N1_bbc','H1N1_guardian','H1N1_reuters','haiti_bbc','IraqWar_guardian','LibyaWar_cnn','LibyaWar_reuters','MJ_bbc','SyrianCrisis_bbc','SyrianCrisis_reuters']
+            self.DATA_PATH = '..\..\DataSet\Timeline17\Data'
+            self.METADATA_PATH = '..\..\DataSet\Timeline17\MetaData'
+            self.CURR_PATH = (os.path.dirname(__file__)).replace('/', '\\')
+
+            self.topic_list = ['bpoil_bbc', 'bpoil_foxnews', 'bpoil_guardian', 'bpoil_reuters', 'bpoil_washingtonpost',
+                               'EgyptianProtest_cnn', 'Finan_washingtonpost', 'H1N1_bbc', 'H1N1_guardian',
+                               'H1N1_reuters', 'haiti_bbc', 'IraqWar_guardian', 'LibyaWar_cnn', 'LibyaWar_reuters',
+                               'MJ_bbc', 'SyrianCrisis_bbc', 'SyrianCrisis_reuters']
             self.date_list = []
             self.gold_label = []
-            self.featurevector_list = []
+            self.feature_vector_list = []
 
             self.clf = linear_model.LogisticRegression()
-            
 
-	def create_metadata(self):
+    def create_metadata(self):
 
-		root_path = os.path.join(self.DATA_PATH)
-		metadata_path = os.path.join(self.METADATA_PATH)
-		feat = extract_features.Features()
-		output_var = extract_goldlabel.GoldLabel()
-		
-		for directory in self.topic_list:
-		
-				print "Getting feature vector for ", directory
-				date_list, featurevector_list = feat.create_featurevector(directory)
+        metadata_path = os.path.join(self.CURR_PATH, self.METADATA_PATH)
+        feat = extract_featurevector.Features()
+        output_var = extract_goldlabels.GoldLabel()
 
-				print "Getting gold labels for ", directory
-				gold_label = output_var.get_goldlabel(directory, date_list)
+        for directory in self.topic_list:
 
-				output = open( directory+'date_list.txt', 'wb')
-				pickle.dump(date_list, output)
-				output.close()
-				
-				output = open( directory+'featurevector_list.txt', 'wb')
-				pickle.dump(featurevector_list, output)
-				output.close()
-				
-				output = open( directory+'gold_label.txt', 'wb')
-				pickle.dump(gold_label, output)
-				output.close()
-				
-		pass
+                if  not (os.path.isfile(metadata_path + '\\' + directory + 'date_list.txt') & os.path.isfile(metadata_path + '\\' + directory + 'feature_vector_list.txt')):
+                    print "Getting feature vector for ", directory
+                    date_list, feature_vector_list = feat.create_feature_vector(directory)
 
-	def get_data(self):
+                    output = open(metadata_path + '\\' + directory + 'date_list.txt', 'wb')
+                    pickle.dump(date_list, output)
+                    output.close()
 
-		root_path = os.path.join(self.METADATA_PATH)
-		feat = extract_features.Features()
-		output = extract_goldlabel.GoldLabel()
-		
-		for directory in self.topic_list:
+                    output = open(metadata_path + '\\' + directory + 'feature_vector_list.txt', 'wb')
+                    pickle.dump(feature_vector_list, output)
+                    output.close()
 
-			input1 = open(directory+'date_list.txt', 'rb')
-			date_list = pickle.load(input1)
-			input1.close()
+                if not os.path.isfile(metadata_path + '\\' + directory + 'gold_label.txt'):
+                    print "Getting gold labels for ", directory
+                    gold_label = output_var.get_goldlabel(directory)
 
-			input1 = open(directory+'featurevector_list.txt', 'rb')
-			featurevector_list = pickle.load(input1)
-			input1.close()
+                    output = open(metadata_path + '\\' + directory + 'gold_label.txt', 'wb')
+                    pickle.dump(gold_label, output)
+                    output.close()
 
-			input1 = open(directory+'gold_label.txt', 'rb')
-			gold_label = pickle.load(input1)
-			input1.close()
+        pass
 
-			if directory != 'SyrianCrisis_reuters':
-					print "Training on ", directory
-					self.train_classifier(featurevector_list, gold_label, date_list)
-			else:
-					self.featurevector_list = featurevector_list
-					self.date_list = date_list
-					self.gold_label = gold_label
+    def get_data(self):
 
-	def train_classifier(self, featurevector_list, gold_label, date_list):
+        root_path = os.path.join(self.CURR_PATH, self.METADATA_PATH)
 
-		self.clf.fit(featurevector_list, gold_label)
+        for directory in self.topic_list:
 
-		pass
+            input1 = open('{0}\\{1}date_list.txt'.format(root_path, directory), 'rb')
+            date_list = pickle.load(input1)
+            input1.close()
 
-	def test_classifier(self):
+            input1 = open('{0}\\{1}feature_vector_list.txt'.format(root_path, directory), 'rb')
+            feature_vector_list = pickle.load(input1)
+            input1.close()
 
-		ypred = self.clf.predict(self.featurevector_list)
-		print "Accuracy Score", metrics.accuracy_score(self.gold_label, ypred)
-	
-		pass
+            input1 = open('{0}\\{1}gold_label.txt'.format(root_path, directory), 'rb')
+            gold_label = pickle.load(input1)
+            input1.close()
+
+            if directory != 'SyrianCrisis_reuters':
+                    print "Training on ", directory
+                    self.train_classifier(feature_vector_list, gold_label)
+            else:
+                    self.feature_vector_list = feature_vector_list
+                    self.date_list = date_list
+                    self.gold_label = gold_label
+
+    def train_classifier(self, feature_vector_list, gold_label):
+
+        self.clf.fit(feature_vector_list, gold_label)
+
+        pass
+
+    def test_classifier(self):
+
+        predict_vector = self.clf.predict(self.feature_vector_list)
+        print "Accuracy Score", metrics.accuracy_score(self.gold_label, predict_vector)
+
+        pass
+
+if __name__ == '__main__':
+    test = DateExtraction()
+    test.create_metadata()
+    test.get_data()
+    test.test_classifier()
