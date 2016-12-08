3e8ecf612fae4e8a84fe8a951dac49d9f527c88e
kruti3 <kruti.chauhan.93@gmail.com>
2016-12-07 20:45:51 -0500

    Update extract_goldlabels.py

--- Phase1/extract_goldlabels.py
+++ Phase1/extract_goldlabels.py
@@ -1,47 +1,50 @@
 import os
 import re
 
+
 class GoldLabel:
+    def __init__(self):
 
+        self.DATA_PATH = '..\..\DataSet\Timeline17\Data'
+        self.OUTPUT_PATH = 'timelines'
+        self.CURR_PATH = (os.path.dirname(__file__)).replace('/', '\\')
+        self.INPUT_PATH = 'InputDocs'
+        self.OUTPUT_VECTOR = []
 
-        def __init__(self):
-            
-            self.DATA_PATH = '\DataSet\Timeline17\Data'
-            self.OUTPUT_PATH = 'timelines'
-            self.INPUT_PATH = 'InputDocs'
-            self.OUTPUT_VECTOR = []
+    def get_goldlabel(self, topic_name, date_list=[]):
 
-        def get_goldlabel(self, topic_name, date_list=[]):
-            
-            root_path = os.path.join(self.DATA_PATH, topic_name, self.OUTPUT_PATH)
+        root_path = os.path.join(self.CURR_PATH, self.DATA_PATH, topic_name, self.OUTPUT_PATH)
 
-            if len(date_list) == 0: 
-                self.create_date_list(topic_name, date_list)
-            
-            self.OUTPUT_VECTOR = [0] * len(date_list)
+        if len(date_list) == 0:
+            self.create_date_list(topic_name, date_list)
 
-            for root, directories, files in os.walk(root_path):
-                for filename in files:
-                    file_path = os.path.join(root, filename)
-                    self.update_fromonefile(file_path, date_list)
+        self.OUTPUT_VECTOR = [0] * len(date_list)
 
-            return self.OUTPUT_VECTOR
+        for root, directories, files in os.walk(root_path):
+            for filename in files:
+                file_path = os.path.join(root, filename)
+                self.update_from_file(file_path, date_list)
 
-        def create_date_list(self, topic_name, date_list):
+        return self.OUTPUT_VECTOR
 
-            root_path = os.path.join(self.DATA_PATH, topic_name, self.INPUT_PATH)
+    def create_date_list(self, topic_name, date_list):
 
-            for root, directories, files in os.walk(root_path):
-                for directory in directories:
-                    date_list.append(directory)
+        root_path = os.path.join(self.CURR_PATH, self.DATA_PATH, topic_name, self.INPUT_PATH)
 
-        def update_fromonefile(self, file_path, date_list):
-            
-            file_obj = open(file_path, 'r')
+        for root, directories, files in os.walk(root_path):
+            for directory in directories:
+                date_list.append(directory)
 
-            for line in file_obj:
-                str1 = re.findall('\d{4}-\d{2}-\d{2}', line)
-                for one in str1:
-                    if one in date_list:
-                        self.OUTPUT_VECTOR[date_list.index(one)] = 1
-                    
+    def update_from_file(self, file_path, date_list):
+
+        file_obj = open(file_path, 'r')
+
+        for line in file_obj:
+            str1 = re.findall('\d{4}-\d{2}-\d{2}', line)
+            for one in str1:
+                if one in date_list:
+                    self.OUTPUT_VECTOR[date_list.index(one)] = 1
+
+# if __name__ == '__main__':
+#    test = GoldLabel()
+#    print test.get_goldlabel('bpoil_bbc')
