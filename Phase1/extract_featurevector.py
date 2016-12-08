1fd8b7ae2b13c447c0e0848bb7c6dd8f24a75c91
kruti3 <kruti.chauhan.93@gmail.com>
2016-12-07 20:45:15 -0500

    Update extract_featurevector.py

--- Phase1/extract_featurevector.py
+++ Phase1/extract_featurevector.py
@@ -1,43 +1,46 @@
 import os
-import re
 import datefinder
 import datetime
+
 
 class Features:
 
         def __init__(self):
             
-            self.DATA_PATH = '\DataSet\Timeline17\Data'
+            self.DATA_PATH = "..\..\DataSet\Timeline17\Data"
+            self.CURR_PATH = (os.path.dirname(__file__)).replace('/', '\\')
+
             self.INPUT_PATH = 'InputDocs'
 
             self.date_list = []
-            self.featurevector_list = []
+            self.feature_vector_list = []
+            self.num_features = 6
 
-        def create_featurevector(self, topic_name):      
+        def create_feature_vector(self, topic_name):      
 
             self.create_date_list(topic_name)
 
-            self.featurevector_list = [[0 for x in range(6)] for y in range(len(self.date_list)) ] 
+            self.feature_vector_list = [[0 for x in range(self.num_features)] for y in range(len(self.date_list))]
 
             for one_date in self.date_list:
-                root_path = os.path.join(self.DATA_PATH, topic_name, self.INPUT_PATH, one_date)
+                root_path = os.path.join(self.CURR_PATH, self.DATA_PATH, topic_name, self.INPUT_PATH, one_date)
                 
                 number_of_files = 0 
                 for root, directories, files in os.walk(root_path):
                     number_of_files = len(files)
                     for one_file in files:
-                        
                         self.update_featurevector(root_path, one_date, one_file)
                 
                 self.update_f1_feature(one_date, number_of_files)    
-                #self.update_f2_feature(one_date, len(files))    
-                #self.update_f3_feature(one_date, len(files))    
-
-            return self.date_list, self.featurevector_list
+                '''
+                self.update_f2_feature(one_date, len(files))
+                self.update_f3_feature(one_date, len(files))
+                '''
+            return self.date_list, self.feature_vector_list
 
         def create_date_list(self, topic_name):
 
-            root_path = os.path.join(self.DATA_PATH, topic_name, self.INPUT_PATH)
+            root_path = os.path.join(self.CURR_PATH, self.DATA_PATH, topic_name, self.INPUT_PATH)
 
             for root, directories, files in os.walk(root_path):
                 for directory in directories:
@@ -48,7 +51,7 @@ class Features:
             root_path = os.path.join(root_path, filename)
             file_obj = open(root_path, 'r')
 
-            date_referred_list = self.extract_datereferred(file_obj, filename)
+            date_referred_list = self.extract_datereference(file_obj)
             
             self.update_f4_feature(date_referred_list, date)
             self.update_f5_feature(date_referred_list, date)
@@ -58,91 +61,88 @@ class Features:
 
         def update_f1_feature(self, one_date, count):
 
-            #Feature F1 indicates articles published on that date.
+            # Feature F1 indicates articles published on that date.
             if one_date in self.date_list:
-                self.featurevector_list[self.date_list.index(one_date)][0] = count
+                self.feature_vector_list[self.date_list.index(one_date)][0] = count
 
-        def extract_datereferred(self, file_obj, date):
+        def extract_datereference(self, file_obj):
 
             date_referred_list = []
-            
+
             for line in file_obj:
                 str1 = datefinder.find_dates(line)
-                
-                for one in str1:
-                    if self.valid_date(one):
-                        one_unaware = one.replace(tzinfo=None)
-                        date_referred_list.append(one_unaware)
-              
+                while True:
+                        try:
+                            one, flag = self.valid_date((str1.__iter__()).next())
+                            if flag:
+                                one_unaware = one.replace(tzinfo=None)
+                                date_referred_list.append(one_unaware)
+                        except ValueError:
+                            continue
+                        except StopIteration:
+                            break
+
             return date_referred_list
 
         def valid_date(self, str1):
 
-            
             if str1.year < 2000 or str1.year > 2015:
-                return 0
+                return str1, 0
 
             if str1.month > 12 or str1.month < 1:
-                return 0
+                return str1, 0
 
             if str1.date > 31 or str1.date < 1:
-                return 0
-            return 1
-            '''
-            try:
-                datetime.datetime.strftime(str1, "%Y-%m-%d")
-               
-            except ValueError:
-                return 0
-                pass
-            except TypeError:
-                return 0
-                pass
-            return 1'''
+                return str1, 0
+
+
+            return str1, 1
             pass
         
         def update_f4_feature(self, date_referred_list, datecurr):
-            
+
+            # sentences published on d and refer to d.
+
             datecurr_fmt = self.str_to_dtfmt(datecurr)
             
-            #sentences published on d and refer to d.
             for one_date in date_referred_list:
                 if self.valid_date(one_date):
-                    one_date_str = datetime.datetime.strftime(one_date,"%Y-%m-%d")
+                    one_date_str = datetime.datetime.strftime(one_date, "%Y-%m-%d")
                     if one_date_str in self.date_list:
-                        if(datecurr_fmt == one_date) :
-                            self.featurevector_list[self.date_list.index(one_date_str)][3] += 1
+                        if datecurr_fmt == one_date:
+                            self.feature_vector_list[self.date_list.index(one_date_str)][3] += 1
 
         def update_f5_feature(self, date_referred_list, datecurr):
-            
+
+            # sentences published after d and refer to d.
+
             datecurr_fmt = self.str_to_dtfmt(datecurr)
 
-            #sentences published after d and refer to d.
             for one_date in date_referred_list:
                 if self.valid_date(one_date):
-                    one_date_str = datetime.datetime.strftime(one_date,"%Y-%m-%d")
+                    one_date_str = datetime.datetime.strftime(one_date, "%Y-%m-%d")
                     if one_date_str in self.date_list:
-                        if(datecurr_fmt > one_date) :
-                            self.featurevector_list[self.date_list.index(one_date_str)][4] += 1
-
+                        if datecurr_fmt > one_date:
+                            self.feature_vector_list[self.date_list.index(one_date_str)][4] += 1
 
         def update_f6_feature(self, date_referred_list, datecurr):
 
+            # sentences published before d and refer to d.
+
             datecurr_fmt = self.str_to_dtfmt(datecurr)
 
-            #sentences published before d and refer to d.
             for one_date in date_referred_list:
                 if self.valid_date(one_date):
-                    one_date_str = datetime.datetime.strftime(one_date,"%Y-%m-%d")
+                    one_date_str = datetime.datetime.strftime(one_date, "%Y-%m-%d")
                     if one_date_str in self.date_list:
-                        if(datecurr_fmt < one_date) :
-                            self.featurevector_list[self.date_list.index(one_date_str)][5] += 1
+                        if datecurr_fmt < one_date:
+                            self.feature_vector_list[self.date_list.index(one_date_str)][5] += 1
 
         def str_to_dtfmt(self, datecurr):
 
             year = int(datecurr[0:4])
             month = int(datecurr[5:7])
             date_n = int(datecurr[8:10])
-            datecurr_fmt = datetime.datetime(year, month, date_n,0,0,0,0)
+            datecurr_fmt = datetime.datetime(year, month, date_n, 0, 0, 0, 0)
 
             return datecurr_fmt
