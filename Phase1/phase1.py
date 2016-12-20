47683954359221a703378045c95858b9de5447be
kruti3 <kruti.chauhan.93@gmail.com>
2016-12-20 11:25:00 -0500

    Added Leave one out approach

--- Phase1/phase1.py
+++ Phase1/phase1.py
@@ -18,6 +18,13 @@ class DateExtraction:
                                'EgyptianProtest_cnn', 'Finan_washingtonpost', 'H1N1_bbc', 'H1N1_guardian',
                                'H1N1_reuters', 'haiti_bbc', 'IraqWar_guardian', 'LibyaWar_cnn', 'LibyaWar_reuters',
                                'MJ_bbc', 'SyrianCrisis_bbc', 'SyrianCrisis_reuters']
+            '''
+            Finan_washingtonpost:1235.txt:ThirdLine:The technology-heavy Nasdaq composite index slid 9.14 percent ,
+            or 199.61 , to 1983.73 , and the broader Standard & Poor 's 500-stock index lost 8.79 percent , or 106.62 ,
+            to close at 1106.39 .
+            Egyptian protest cnn: Output.txt
+            removed 2011-02-12
+            '''
             self.date_list = []
             self.gold_label = []
             self.feature_vector_list = []
@@ -32,21 +39,24 @@ class DateExtraction:
 
         for directory in self.topic_list:
 
-                if  not (os.path.isfile(metadata_path + '\\' + directory + 'date_list.txt') & os.path.isfile(metadata_path + '\\' + directory + 'feature_vector_list.txt')):
+                if  not (os.path.isfile(metadata_path + '\\' + directory + 'date_list.txt') & os.path.isfile(metadata_path + '\\' + directory + 'feature_vector.txt')):
                     print "Getting feature vector for ", directory
                     date_list, feature_vector_list = feat.create_feature_vector(directory)
-
                     output = open(metadata_path + '\\' + directory + 'date_list.txt', 'wb')
                     pickle.dump(date_list, output)
                     output.close()
 
-                    output = open(metadata_path + '\\' + directory + 'feature_vector_list.txt', 'wb')
+                    output = open(metadata_path + '\\' + directory + 'feature_vector.txt', 'wb')
                     pickle.dump(feature_vector_list, output)
                     output.close()
 
                 if not os.path.isfile(metadata_path + '\\' + directory + 'gold_label.txt'):
                     print "Getting gold labels for ", directory
-                    gold_label = output_var.get_goldlabel(directory)
+                    input1 = open('{0}\\{1}date_list.txt'.format(metadata_path, directory), 'rb')
+                    date_list = pickle.load(input1)
+                    input1.close()
+
+                    gold_label = output_var.get_goldlabel(directory, date_list)
 
                     output = open(metadata_path + '\\' + directory + 'gold_label.txt', 'wb')
                     pickle.dump(gold_label, output)
@@ -54,7 +64,7 @@ class DateExtraction:
 
         pass
 
-    def get_data(self):
+    def get_data(self, test_data):
 
         root_path = os.path.join(self.CURR_PATH, self.METADATA_PATH)
 
@@ -64,7 +74,7 @@ class DateExtraction:
             date_list = pickle.load(input1)
             input1.close()
 
-            input1 = open('{0}\\{1}feature_vector_list.txt'.format(root_path, directory), 'rb')
+            input1 = open('{0}\\{1}feature_vector.txt'.format(root_path, directory), 'rb')
             feature_vector_list = pickle.load(input1)
             input1.close()
 
@@ -72,7 +82,7 @@ class DateExtraction:
             gold_label = pickle.load(input1)
             input1.close()
 
-            if directory != 'SyrianCrisis_reuters':
+            if directory != test_data:
                     print "Training on ", directory
                     self.train_classifier(feature_vector_list, gold_label)
             else:
@@ -89,12 +99,21 @@ class DateExtraction:
     def test_classifier(self):
 
         predict_vector = self.clf.predict(self.feature_vector_list)
-        print "Accuracy Score", metrics.accuracy_score(self.gold_label, predict_vector)
+        acc = metrics.accuracy_score(self.gold_label, predict_vector)
+        print "Accuracy Score", acc
 
+        return acc
         pass
 
 if __name__ == '__main__':
     test = DateExtraction()
     test.create_metadata()
-    test.get_data()
-    test.test_classifier()
+
+    acc = 0
+    for test_data in test.topic_list:
+        test.get_data(test_data)
+        print "Predicting for", test_data
+        acc += test.test_classifier()
+
+    acc = acc*1.0/len(test.topic_list)
+    print "Average Accuracy Score", acc
